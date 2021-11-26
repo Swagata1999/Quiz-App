a868d7a85434483fcd6c61702a957a3603f59762
Swagata1999 <swagatadas1999@gmail.com>
2021-11-26 15:41:32 +0530

    Commit

---
+++ src/helpers/Questions.js
@@ -0,0 +1,43 @@
+export const Questions = [
+    {
+        prompt: "What is my name?",
+        optionA: "John",
+        optionB: "Jake",
+        optionC: "Swagata",
+        optionD: "Jake",
+        asnwer: "optionC",
+    },
+    {
+        prompt: "What is my pet name?",
+        optionA: "puppy",
+        optionB: "milky",
+        optionC: "honey",
+        optionD: "chiku",
+        asnwer: "optionD",
+    },
+    {
+        prompt: "Which of this is not a programming language?",
+        optionA: "Python",
+        optionB: "JavaScript",
+        optionC: "MC-03",
+        optionD: "Java",
+        asnwer: "optionC",
+    },
+    {
+        prompt: "Which of this is not a javascript framework?",
+        optionA: "React",
+        optionB: "Angular",
+        optionC: "Vue",
+        optionD: "Java",
+        asnwer: "optionD",
+    },
+    {
+        prompt:" What is the full form of SQL?",
+
+        optionA:"Structured Query List",
+        optionB: "Structure Query Language",
+        optionC:"Sample Query Language",
+        optionD:  "None of these",
+        asnwer: "optionB",
+    },
+];
